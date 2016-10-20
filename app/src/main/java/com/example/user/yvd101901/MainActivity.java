998b11cdc1eef1002d4a77cb3e8fabab5bc292a1
Shaojung <shaojung@gmail.com>
2016-10-20 09:47:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/yvd101901/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.user.yvd101901;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("ACT", "This is onCreate");
+    }
+
+    public void click1(View v)
+    {
+        Log.d("ACT", "按鈕被按下");
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText("Button clicked!!");
+    }
+
+    public void click2(View v)
+    {
+        EditText ed = (EditText) findViewById(R.id.editText);
+        String str = ed.getText().toString();
+        TextView tv3 = (TextView) findViewById(R.id.textView3);
+        tv3.setText(str);
+    }
+}
